9baf54dbf2bbdb6f8c9660316b561eabf9fcc775
mathew owusu jr <mrfriiday@gmail.com>
2022-05-15 02:31:05 -0400

    relationship with cart and reserved shoes working

--- website/models.py
+++ website/models.py
@@ -4,22 +4,40 @@ from . import db
 # from flask_login import UserMixin
 
 class Customer(db.Model):
+    __tablename__ = 'customer'
     id = db.Column(db.Integer(), primary_key=True)
+    # displayname = db.Column()
+    # email = db.Column()
+    # cartid = db.Column()
 
 
 class Cart(db.Model):
+    __tablename__ = 'cart'
     id = db.Column(db.Integer(), primary_key=True)
-
+    # custid = db.Column(db.Integer(), db.ForeignKey('customer.id')) # one to one
+    
+    # shoe_id = db.Column(db.Integer(), db.ForeignKey('shoe.id')) # one to many
+    shoes = db.relationship("ReservedShoe", back_populates="cart", lazy=True)
+    def __repr__(self):
+        return '<Cart {},shoes={}'.format(self.id, self.shoes)
 
 class ReservedShoe(db.Model):
     __tablename__ = 'reserved_shoe'
     id = db.Column(db.Integer(), primary_key=True)
+    cartid = db.Column(db.Integer(), db.ForeignKey('cart.id'))
+    cart = db.relationship('Cart', back_populates='shoes')
+
     shoe_id = db.Column(db.Integer(), db.ForeignKey('shoe.id')) # one to one
     reserved_shoe = db.relationship("Shoe", backref="reserved_shoe", uselist=False)
     quantity = db.Column(db.Integer(), nullable=False)
+    def __repr__(self):
+        return '<ReservedShoe {}, cartid={}, shoeid={}, quan={}'.format(self.id, self.cartid,
+        self.shoe_id, self.quantity)
+    
 
 
 class Shoe(db.Model):
+    __tablename__ = 'shoe'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False)
     brand = db.Column(db.String(length=30), nullable=False)
@@ -27,3 +45,5 @@ class Shoe(db.Model):
     size = db.Column(db.Float(), nullable=False)
     quantity = db.Column(db.Integer(), nullable=False)
     price = db.Column(db.Float(), nullable=False)
+    def __repr__(self):
+        return '<Shoe {}, name={}'.format(self.id, self.name)
--- website/views/inventory.py
+++ website/views/inventory.py
@@ -1,15 +1,15 @@
 from select import select
 from flask import Blueprint, render_template, redirect
-from ..models import ReservedShoe, Shoe
+from ..models import ReservedShoe, Shoe, Cart
 inventory_view = Blueprint('inventory_view', __name__)
 from .. import db
 
 @inventory_view.route('/inventory')
 def inventory():
     new_shoe = Shoe(
-        name="Yeezy Boost 350",
+        name="Yeezy Boost 700",
         brand="Yeezy",
-        color="Zebra",
+        color="Black",
         size=9.5,
         quantity=100,
         price=250.00
@@ -17,11 +17,23 @@ def inventory():
     db.session.add(new_shoe)
     db.session.commit()
 
-    new_shoe = db.session.query(Shoe).first()
+    # new_cart = Cart()
+    # db.session.add(new_cart)
+    # db.session.commit()
+
+    new_cart = db.session.query(Cart).first()
+    new_shoe = Shoe.query.filter_by(id=2).first()
     reserved_shoe = ReservedShoe(
+        cartid=new_cart.id,
         shoe_id=new_shoe.id,
         quantity=1
     )
     db.session.add(reserved_shoe)
     db.session.commit()
-    return "Hello Inventory"
+
+    new_cart.shoes.append(reserved_shoe)
+    db.session.add(new_cart)
+    db.session.commit()
+    print(db.session.query(Cart).first())
+    print_cart = db.session.query(Cart).first()
+    return print_cart.__repr__()
