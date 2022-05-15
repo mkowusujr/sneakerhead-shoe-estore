280b3f8852fb819bc215df46d7453bbc2f3687f8
mathew owusu jr <mrfriiday@gmail.com>
2022-05-15 02:41:57 -0400

    removed test code

--- website/views/inventory.py
+++ website/views/inventory.py
@@ -6,34 +6,4 @@ from .. import db
 
 @inventory_view.route('/inventory')
 def inventory():
-    new_shoe = Shoe(
-        name="Yeezy Boost 700",
-        brand="Yeezy",
-        color="Black",
-        size=9.5,
-        quantity=100,
-        price=250.00
-    )
-    db.session.add(new_shoe)
-    db.session.commit()
-
-    # new_cart = Cart()
-    # db.session.add(new_cart)
-    # db.session.commit()
-
-    new_cart = db.session.query(Cart).first()
-    new_shoe = Shoe.query.filter_by(id=2).first()
-    reserved_shoe = ReservedShoe(
-        cartid=new_cart.id,
-        shoe_id=new_shoe.id,
-        quantity=1
-    )
-    db.session.add(reserved_shoe)
-    db.session.commit()
-
-    new_cart.shoes.append(reserved_shoe)
-    db.session.add(new_cart)
-    db.session.commit()
-    print(db.session.query(Cart).first())
-    print_cart = db.session.query(Cart).first()
-    return print_cart.__repr__()
+    return "Hello World"
