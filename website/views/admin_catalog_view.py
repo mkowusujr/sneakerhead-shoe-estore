b8be288133307b4849e02447c23a05cfc1f10192
mathew owusu jr <mrfriiday@gmail.com>
2022-05-15 23:56:49 -0400

    can add to inventory

--- website/views/admin_catalog_view.py
+++ website/views/admin_catalog_view.py
@@ -1,10 +1,27 @@
 from select import select
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
 from ..models import ReservedShoe, Shoe, Cart
 admin_catalog_view = Blueprint('admin_catalog_view', __name__)
 from .. import db
 
-@admin_catalog_view.route('/inventory')
-def inventory_page():
+@admin_catalog_view.route('/inventory', methods=['GET'])
+def display_inventory():
     inventory = Shoe.query.all()
     return render_template('admin_catalog.html', inventory=inventory)
+    
+
+@admin_catalog_view.route('/inventory', methods=['POST'])
+def add_to_inventory():
+    new_shoe = Shoe(
+        name = request.form['name'],
+        brand = request.form['brand'],
+        color = request.form['color'],
+        size = request.form['size'],
+        quantity = request.form['quantity'],
+        price = request.form['price']
+    )
+
+    db.session.add(new_shoe)
+    db.session.commit()
+
+    return redirect('/inventory')
