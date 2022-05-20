0d523902a0b4cd76f2292c5d11b05c248fd8171c
mathew owusu jr <mrfriiday@gmail.com>
2022-05-20 16:54:03 -0400

    can see specific brands, or broswer entire site

--- website/views/cust_catalog_views.py
+++ website/views/cust_catalog_views.py
@@ -4,26 +4,43 @@ from ..models import ReservedShoe, Shoe, Cart
 cust_catalog_views = Blueprint('cust_catalog_views', __name__)
 from .. import db
 
-@cust_catalog_views.route('/releases')
+@cust_catalog_views.route('/releases', methods=['GET'])
 def releases_page():
     title = "New Releases"
     collection = Shoe.query.all()
     return render_template('catalog.html', title=title, collection=collection)
 
-@cust_catalog_views.route('/mens/releases')
+@cust_catalog_views.route('/mens/releases', methods=['GET'])
 def mens_releases_page():
     title = "New Releases"
     collection = Shoe.query.filter_by(audience="Men")
     return render_template('catalog.html', title=title, collection=collection)
 
-@cust_catalog_views.route('/womens/releases')
+@cust_catalog_views.route('/womens/releases', methods=['GET'])
 def womens_releases_page():
     title = "New Releases"
     collection = Shoe.query.filter_by(audience="Womens")
     return render_template('catalog.html', title=title, collection=collection)
 
-@cust_catalog_views.route('/kids/releases')
+@cust_catalog_views.route('/kids/releases', methods=['GET'])
 def kids_releases_page():
     title = "New Releases"
     collection = Shoe.query.filter_by(audience="Kids")
+    return render_template('catalog.html', title=title, collection=collection)
+
+@cust_catalog_views.route('/<string:brand>/releases', methods=['GET'])
+def brand_releases_page(brand):
+    title = brand + " New Releases"
+    collection = Shoe.query.filter_by(brand=brand)
+    return render_template('catalog.html', title=title, collection=collection)
+
+@cust_catalog_views.route('/releases/browse/<string:searchQuery>')
+def search_any_product(searchQuery):
+    title = "Sneaker Headz Releases"
+    collection = []
+    similar_names = Shoe.query.filter(Shoe.name.like('%'+searchQuery+'%')).all()
+    similar_brands = Shoe.query.filter(Shoe.brand.like('%'+searchQuery+'%')).all()
+    collection.extend(similar_names)
+    collection.extend(similar_brands)
+    # collection.ppend()
     return render_template('catalog.html', title=title, collection=collection)
