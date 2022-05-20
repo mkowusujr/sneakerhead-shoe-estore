2c04dd3d70b05f20066eb163fcf1b33e171ada79
mathew owusu jr <mrfriiday@gmail.com>
2022-05-20 16:53:29 -0400

    added brands list to homepage

--- website/views/home_view.py
+++ website/views/home_view.py
@@ -8,4 +8,11 @@ from .. import db
 @home_view.route('/home')
 def home_page():
     recently_added = Shoe.query.order_by(Shoe.id.desc()).limit(5)
-    return render_template("home.html", recently_added=recently_added)
+
+    shoes = Shoe.query.all()
+    brands = []
+    for shoe in shoes:
+        if not brands.__contains__(shoe.brand):
+            brands.append(shoe.brand)
+
+    return render_template("home.html", recently_added=recently_added, brands=brands)
