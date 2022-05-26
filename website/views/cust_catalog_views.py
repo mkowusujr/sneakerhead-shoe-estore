30051693892cb600472364e40b3e21b4f8485b8c
mathew owusu jr <mrfriiday@gmail.com>
2022-05-25 21:35:19 -0400

    pagination working for all catalog views

--- website/views/cust_catalog_views.py
+++ website/views/cust_catalog_views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request
 from flask_login import current_user
 from ..models import Shoe
+
+
+PER_PAGE = 36
 cust_catalog_views = Blueprint('cust_catalog_views', __name__)
 
 
@@ -8,54 +11,90 @@ cust_catalog_views = Blueprint('cust_catalog_views', __name__)
 def releases_page():
     title = "New Releases"
     page = request.args.get('page', 1, type=int)
-    PER_PAGE = 36
     collection = Shoe.query.paginate(page=page, per_page=PER_PAGE)
-    return render_template('customer/catalog.html', current_user=current_user, title=title, collection=collection)
+    routeUrl = 'cust_catalog_views.releases_page'
+
+    return render_template('customer/catalog.html', 
+    current_user=current_user, 
+    title=title, 
+    collection=collection, 
+    routeUrl=routeUrl)
 
 
 @cust_catalog_views.route('/mens/releases', methods=['GET'])
 def mens_releases_page():
     title = "New Releases"
-    collection = Shoe.query.filter_by(audience="Men")
-    return render_template('customer/catalog.html', title=title, collection=collection)
+    page = request.args.get('page', 1, type=int)
+    collection = Shoe.query.filter_by(audience="Men").paginate(page=page, per_page=PER_PAGE)
+    routeUrl = 'cust_catalog_views.mens_releases_page'
+
+    return render_template('customer/catalog.html', 
+    title=title, 
+    collection=collection, 
+    routeUrl=routeUrl)
 
 
 @cust_catalog_views.route('/womens/releases', methods=['GET'])
 def womens_releases_page():
     title = "New Releases"
-    collection = Shoe.query.filter_by(audience="Women")
-    return render_template('customer/catalog.html', title=title, current_user=current_user, collection=collection)
+    page = request.args.get('page', 1, type=int)
+    collection = Shoe.query.filter_by(audience="Women").paginate(page=page, per_page=PER_PAGE)
+    routeUrl = 'cust_catalog_views.womens_releases_page'
+
+    return render_template('customer/catalog.html', 
+    title=title, 
+    current_user=current_user, 
+    collection=collection, 
+    routeUrl=routeUrl)
 
 
 @cust_catalog_views.route('/kids/releases', methods=['GET'])
 def kids_releases_page():
     title = "New Releases"
-    collection = Shoe.query.filter_by(audience="Children")
-    return render_template('customer/catalog.html', title=title, current_user=current_user, collection=collection)
+    page = request.args.get('page', 1, type=int)
+    collection = Shoe.query.filter_by(audience="Children").paginate(page=page, per_page=PER_PAGE)
+    routeUrl = 'cust_catalog_views.kids_releases_page'
+
+    return render_template('customer/catalog.html', 
+    title=title, 
+    current_user=current_user, 
+    collection=collection, 
+    routeUrl=routeUrl)
 
 
 @cust_catalog_views.route('/<string:brand>/releases', methods=['GET'])
 def brand_releases_page(brand):
     title = brand + " New Releases"
-    collection = Shoe.query.filter_by(brand=brand)
-    return render_template('customer/catalog.html', title=title, current_user=current_user, collection=collection)
+    page = request.args.get('page', 1, type=int)
+    collection = Shoe.query.filter_by(brand=brand).paginate(page=page, per_page=PER_PAGE)
+    routeUrl = 'cust_catalog_views.brand_releases_page'
+
+    return render_template('customer/catalog.html', 
+    title=title, 
+    current_user=current_user, 
+    collection=collection, 
+    routeUrl=routeUrl,
+    brand=brand)
 
 
 @cust_catalog_views.route('/releases/browse/<string:searchQuery>')
 def search_any_product(searchQuery):
     title = "Sneaker Headz Releases"
-    collection = []
+    page = request.args.get('page', 1, type=int)
+    collection = Shoe.query.filter(Shoe.name.like('%'+searchQuery+'%') | Shoe.brand.like('%'+searchQuery+'%')).paginate(page=page, per_page=PER_PAGE)
+    routeUrl = 'cust_catalog_views.search_any_product'
 
-    similar_names = Shoe.query.filter(Shoe.name.like('%'+searchQuery+'%')).all()
-    similar_brands = Shoe.query.filter(Shoe.brand.like('%'+searchQuery+'%')).all()
-    collection.extend(similar_names)
-    collection.extend(similar_brands)
-    
-    return render_template('customer/catalog.html', title=title, collection=collection)
+    return render_template('customer/catalog.html', 
+    title=title, 
+    collection=collection, 
+    routeUrl=routeUrl,
+    searchQuery=searchQuery)
 
 
 @cust_catalog_views.route('/product/<int:id>', methods=['GET'])
 def display_product(id):
     shoe = Shoe.query.get_or_404(id)
-    return render_template('customer/product.html', current_user=current_user, shoe=shoe)
+    return render_template('customer/product.html', 
+    current_user=current_user, 
+    shoe=shoe)
     
